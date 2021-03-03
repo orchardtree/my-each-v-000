6d702ed93e6a7304f14385af58d055a99f1eb6f0
Jesse Wenger <wengerj@gmail.com>
2021-03-03 15:18:56 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
+  i = 0
+  while i < array.length 
+    yield(array[i])
+  end
+end
+
+my_each([1, 2, 3, 4]) do |item|
+  puts item
 end
